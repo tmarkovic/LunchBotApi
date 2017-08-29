5adf784d806198b8ad9c481a5651d3c01616ee5e
tmarkovic <tmarkovic@ttmedia.se>
2017-08-29 18:53:43 +0200

    changed restaurants to HTTP GET

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,7 +6,7 @@ var url =
   "http://helagotland.se/lunchguiden/?city=TWsm&rest=hMTp_sLUa_VfSA_VfS9_tCUE_qqvn_gxSp_cPVn_VfRl_qZGW_VfRs_VEN7_VfRy_wGaL_wrB1_qr0n_VEN3_nqA8_YyJR_VfSD_jswO_wAoz_Vxe5_VfRu_jeTu_VEN5_omZp_VfS3_VfS8";
 
 /* GET users listing. */
-router.post("/", function(req, res, next) {
+router.get("/", function(req, res, next) {
   fetchRestaurants(url).then(x => res.json(x));
 });
 
